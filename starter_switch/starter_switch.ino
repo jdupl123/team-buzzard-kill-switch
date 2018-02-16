f771dd2af2b5494c92b35d0b770e9b4a75352456
John Vial <badwolf.johnnyv@gmail.com>
2018-02-17 07:33:25 +0800

    Added starters switch using serial

---
+++ starter_switch/starter_switch.ino
@@ -0,0 +1,31 @@
+
+int STARTER_PIN_OUT= 32;
+
+void setup() {
+  // put your setup code here, to run once:
+  // initialize serial:
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  while (Serial.available() > 0) {
+    
+    // Grab an input, we expect a 1
+    int input = Serial.parseInt();
+    String blah = Serial.readString();
+
+    if (input == 1) 
+    {
+      // Set starter pin to high  
+
+      digitalWrite(STARTER_PIN_OUT, HIGH);
+      // Wait for car to start second
+      delay(1);
+
+      // Set starter pin to low
+      digitalWrite(STARTER_PIN_OUT, LOW);
+    }
+    
+  }
+}
