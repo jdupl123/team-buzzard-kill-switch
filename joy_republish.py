18bef06d1a7d10707a8f517511df60df298393ff
UeliGraf <grafue@student.ethz.ch>
2018-02-17 14:49:23 +0100

    safe gear change

--- joy_republish.py
+++ joy_republish.py
@@ -24,14 +24,14 @@ class publisher(object):
         self.brake_msg = max(0,255*joy_data.axes[1]*-1)
         self.throttle_msg = max(0,255*joy_data.axes[1])
 
-        if (joy_data.buttons[7] > 0) :
-            if joy_data.axes[7] == -1:
+        if (joy_data.buttons[7] > 0 and joy_data.axes[1] < 0.9) :
+            if joy_data.axes[6] == -1:
                 self.gear_msg = 80  # P
-            elif joy_data.axes[7] == 1:
+            elif joy_data.axes[6] == 1:
                 self.gear_msg = 78  # N
-            elif joy_data.axes[8] == 1:
+            elif joy_data.axes[7] == 1:
                 self.gear_msg = 68  # D
-            elif joy_data.axes[8] == -1:
+            elif joy_data.axes[7] == -1:
                 self.gear_msg = 82  # R
 
         if joy_data.buttons[6] > 0:
