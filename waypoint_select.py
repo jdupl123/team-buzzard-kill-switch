9442df3c901641ab88c8a43adc3ff52e313f6bc5
UeliGraf <grafue@student.ethz.ch>
2018-02-18 01:42:44 +0100

    Architecture up and running

--- waypoint_select.py
+++ waypoint_select.py
@@ -4,29 +4,30 @@ from nav_msgs.msg import Odometry
 import numpy as np
 from scipy import spatial
 
-def state_callback( state ):
-    position = np.array([[state.pose.pose.position.x, state.pose.pose.position.y]])
-    heading = state.pose.pose.position.z
-    waypointSelect(position, heading)
-
-state_sub = rospy.Subscriber('state', Odometry, state_callback)
-target_pub = rospy.Publisher('target', Odometry, queue_size=10)
+rospy.init_node('waypoint_selector', anonymous=False)
 
 def waypointSelect(currentPos, heading):
     """
     This function returns the target GPS coordinates, either a list of waypoints or an estimate from the vision system.
     The heading is also used to ensure that the waypoints behind the car are excluded
     """
-    gpsCoords = np.array([
-        [-27.8552175, 153.1511374],
-        [-27.8554650, 153.1516188],
-        [-27.8557407, 153.1520735],
-        [-27.8560205, 153.1521694],
-        [-27.8560920, 153.1519464],
-        [-27.8559772, 153.1513396],
-        [-27.8555771, 153.1508628],
-        [-27.8553002, 153.1509104]
+    gpsCoords = np.array([ #fake
+        [27.511888, 153.089826],
+        [27.511728, 153.089782],
+        [27.511798, 153.089667],
     ])
+
+    #gpsCoords = np.array([ #real
+    #    [-27.8552175, 153.1511374],
+    #    [-27.8554650, 153.1516188],
+    #    [-27.8557407, 153.1520735],
+    #    [-27.8560205, 153.1521694],
+    #    [-27.8560920, 153.1519464],
+    #    [-27.8559772, 153.1513396],
+    #    [-27.8555771, 153.1508628],
+    #    [-27.8553002, 153.1509104]
+    #])
+
     DIST_THRESHOLD = 10
 
     gpsDist = spatial.distance.cdist(gpsCoords, currentPos)
@@ -38,16 +39,23 @@ def waypointSelect(currentPos, heading):
     gpsMinAngle = gpsAngle[minind]
 
     print (gpsMinX, gpsMinY, gpsMinAngle)
-
     msg = Odometry()
-    msg.pose.pose.position.x = 0
-    msg.pose.pose.position.y = 0
+    msg.pose.pose.position.x = gpsMinX
+    msg.pose.pose.position.y = gpsMinY
     msg.pose.pose.position.z = gpsMinAngle
     msg.twist.twist.linear.z = 10
-
     target_pub.publish(msg)
-rospy.init_node('waypoint_selector', anonymous=False)
+
+def state_callback( state ):
+    position = np.array([[state.pose.pose.position.x, state.pose.pose.position.y]])
+    heading = state.pose.pose.position.z
+    waypointSelect(position, heading)
+
+state_sub = rospy.Subscriber('state', Odometry, state_callback)
+target_pub = rospy.Publisher('target', Odometry, queue_size=10)
+
 rospy.spin()
+
 
 # Dummy data for testing
 
